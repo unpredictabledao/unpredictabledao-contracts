52b78effebcc79f7fae945c034dd3a1c4943f845
Phu Minh <53084840+nguyenphuminh@users.noreply.github.com>
2022-07-11 00:31:48 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,6 +12,17 @@ module.exports = {
       url: `https://arctic-rpc.icenetwork.io:9933`,
       accounts: [`0x${ICE_PRIVATE_KEY}`]
     },
+    acala: {
+      chainId: 595,
+      gasPrice: 429496729610000,
+      url: `https://tc7-eth.aca-dev.network`,
+      accounts: [`0x${ICE_PRIVATE_KEY}`]
+    },
+    astar: {
+      chainId: 81,
+      url: `https://evm.shibuya.astar.network/`,
+      accounts: [`0x${ICE_PRIVATE_KEY}`]
+    },
     hardhat: {
       chainId: 1337
     }
