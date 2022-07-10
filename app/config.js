dc69494f9f4f8df43047e980ffdc46a1be39dd25
Phu Minh <53084840+nguyenphuminh@users.noreply.github.com>
2022-07-11 01:18:03 +0700

    Add files via upload

---
+++ app/config.js
@@ -0,0 +1,690 @@
+const config = {
+  "tokenContractAddress": "0xC2469d565A2DC77f4a496Ff5e3B25ED69Ea8AB6b",
+  "tokenContractABI": [
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "_mintAddress",
+            "type": "address"
+          }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "internalType": "address",
+            "name": "_owner",
+            "type": "address"
+          },
+          {
+            "indexed": true,
+            "internalType": "address",
+            "name": "_spender",
+            "type": "address"
+          },
+          {
+            "indexed": false,
+            "internalType": "uint256",
+            "name": "_value",
+            "type": "uint256"
+          }
+        ],
+        "name": "Approval",
+        "type": "event"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "internalType": "address",
+            "name": "_from",
+            "type": "address"
+          },
+          {
+            "indexed": true,
+            "internalType": "address",
+            "name": "_to",
+            "type": "address"
+          },
+          {
+            "indexed": false,
+            "internalType": "uint256",
+            "name": "_value",
+            "type": "uint256"
+          }
+        ],
+        "name": "Transfer",
+        "type": "event"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "_owner",
+            "type": "address"
+          },
+          {
+            "internalType": "address",
+            "name": "_spender",
+            "type": "address"
+          }
+        ],
+        "name": "allowance",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "remaining",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "_spender",
+            "type": "address"
+          },
+          {
+            "internalType": "uint256",
+            "name": "_value",
+            "type": "uint256"
+          }
+        ],
+        "name": "approve",
+        "outputs": [
+          {
+            "internalType": "bool",
+            "name": "success",
+            "type": "bool"
+          }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "_owner",
+            "type": "address"
+          }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "balance",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "name": "balances",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+          {
+            "internalType": "uint8",
+            "name": "",
+            "type": "uint8"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+          {
+            "internalType": "string",
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+          {
+            "internalType": "string",
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "_to",
+            "type": "address"
+          },
+          {
+            "internalType": "uint256",
+            "name": "_value",
+            "type": "uint256"
+          }
+        ],
+        "name": "transfer",
+        "outputs": [
+          {
+            "internalType": "bool",
+            "name": "success",
+            "type": "bool"
+          }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "_from",
+            "type": "address"
+          },
+          {
+            "internalType": "address",
+            "name": "_to",
+            "type": "address"
+          },
+          {
+            "internalType": "uint256",
+            "name": "_value",
+            "type": "uint256"
+          }
+        ],
+        "name": "transferFrom",
+        "outputs": [
+          {
+            "internalType": "bool",
+            "name": "success",
+            "type": "bool"
+          }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      }
+  ],
+
+  "riceContractAddress": "0xEDBA386C67364084c5eCbb4C50422eEDCfB19374",
+  "riceContractABI": [
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_tokenAddress",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "reward",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "participant",
+          "type": "address"
+        }
+      ],
+      "name": "CollectReward",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "number",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "campaignIndex",
+          "type": "uint256"
+        }
+      ],
+      "name": "GenerateNumber",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "campaignIndex",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "participant",
+          "type": "address"
+        }
+      ],
+      "name": "Reveal",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "campaignIndex",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "participant",
+          "type": "address"
+        }
+      ],
+      "name": "Submit",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "fee",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "participant",
+          "type": "address"
+        }
+      ],
+      "name": "VoteOnCallerFee",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "fee",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "participant",
+          "type": "address"
+        }
+      ],
+      "name": "WithdrawOnCallerFee",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "subIndex",
+          "type": "uint256"
+        }
+      ],
+      "name": "calculateReward",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "campaignIndexes",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "campaignIndexesLength",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "subIndex",
+          "type": "uint256"
+        }
+      ],
+      "name": "collectReward",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "currentCampaign",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "deposits",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "generateNumber",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "participationFee",
+      "outputs": [
+        {
+          "internalType": "uint64",
+          "name": "",
+          "type": "uint64"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "subIndex",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "realValue",
+          "type": "uint256"
+        },
+        {
+          "internalType": "bytes32",
+          "name": "dummyValue",
+          "type": "bytes32"
+        }
+      ],
+      "name": "reveal",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "revealers",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "rewards",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "rngFee",
+      "outputs": [
+        {
+          "internalType": "uint64",
+          "name": "",
+          "type": "uint64"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        },
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "submissions",
+      "outputs": [
+        {
+          "internalType": "bytes32",
+          "name": "mask",
+          "type": "bytes32"
+        },
+        {
+          "internalType": "bool",
+          "name": "revealed",
+          "type": "bool"
+        },
+        {
+          "internalType": "bool",
+          "name": "rewarded",
+          "type": "bool"
+        },
+        {
+          "internalType": "uint8",
+          "name": "flag",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "bytes32",
+          "name": "maskedValue",
+          "type": "bytes32"
+        }
+      ],
+      "name": "submit",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint64",
+          "name": "fee",
+          "type": "uint64"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "voteOnCallerFee",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "votes",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "fee",
+          "type": "uint256"
+        }
+      ],
+      "name": "withdrawOnCallerFee",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]
+}
---
+++ app/main.js
@@ -0,0 +1,213 @@
+// Connect to Metamask
+window.ethereum.request({ method: 'eth_requestAccounts'});
+
+window.ethereum.request({
+    method: "wallet_addEthereumChain",
+    params: [{
+        chainId: "0x228",
+        rpcUrls: ["https://arctic-rpc.icenetwork.io:9933/"],
+        chainName: "Arctic Network",
+        nativeCurrency: {
+            name: "ICY",
+            symbol: "ICY",
+            decimals: 18
+        },
+        blockExplorerUrls: null
+    }]
+});
+
+// Get provider and signer
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner();
+
+// Get contracts' configurations
+const { tokenContractAddress, tokenContractABI, riceContractAddress, riceContractABI  } = config;
+
+// Create contracts' instances
+const token = new ethers.Contract(tokenContractAddress, tokenContractABI, signer);
+const rice = new ethers.Contract(riceContractAddress, riceContractABI, signer);
+
+// Joined campaigns' indexes
+const joinedCampaigns = [];
+
+// Get UGT decimals
+let decimals;
+
+(async () => {
+    decimals = await token.decimals;
+})();
+
+/*
+    PARTICIPATION
+*/
+
+// Values to enter submissions.
+let realValue, dummyValue, maskedValue, doneWithParticipation = true;
+
+rice.on("Submit", async (campaignIndex, participant) => {
+    if (participant === await signer.getAddress()) {
+        alert(`Submitted to campaign #${campaignIndex}, funds locked.`);
+
+        // Reveal right after submission
+        await rice.reveal(campaignIndex, realValue, dummyValue);
+
+        doneWithParticipation = true;
+    }
+});
+
+rice.on("Reveal", async (campaignIndex, participant) => {
+    if (participant === await signer.getAddress()) {
+        alert(`Revealed values for campaign #${campaignIndex}, funds received.`);
+
+        // Save joined campaign indexes.
+        joinedCampaigns.push(BigInt(campaignIndex));
+    }
+});
+
+async function participate() {
+    if ( ethers.BigNumber.from(await signer.getBalance()).lt(ethers.utils.parseEther("5.0")) ) {
+        alert("Not enough money to participate!");
+        return;
+    }
+
+    if (!doneWithParticipation) {
+        alert("Last submission has not finished!");
+        return;
+    };
+
+    realValue = Math.floor(Math.random() * 2147483648);
+    
+    dummyValue = ethers.utils.keccak256(
+        ethers.utils.toUtf8Bytes(
+            Math.floor(Math.random() * 10000).toString()
+        )
+    );
+
+    maskedValue = ethers.utils.solidityKeccak256(
+        ["uint", "bytes32"],
+        [realValue, dummyValue]
+    );
+
+    doneWithParticipation = false;
+
+    await rice.submit(maskedValue, { value: ethers.utils.parseEther("5.0") });
+}
+
+const participateButton = document.querySelector("#participate-button");
+
+participateButton.onclick = async () => {
+    await participate();
+}
+
+
+
+/*
+    REWARD
+*/
+
+rice.on("CollectReward", async (reward, participant) => {
+    if (participant === await signer.getAddress()) {
+        alert("Received reward: " + BigInt(reward).toString());
+    }
+});
+
+async function claim() {
+    const campaignIndex = document.querySelector("#claim-index").value;
+
+    /*if (!joinedCampaigns.includes(BigInt(campaignIndex))) {
+        alert("You did not join the campaign!");
+        return;
+    };*/
+
+    await rice.collectReward(campaignIndex);
+}
+
+const claimButton = document.querySelector("#claim-button");
+
+claimButton.onclick = async () => {
+    await claim();
+}
+
+
+
+/*
+    VOTING
+*/
+
+// Voting
+
+rice.on("VoteOnCallerFee", async (fee, amount, participant) => {
+    if (participant === await signer.getAddress()) {
+        alert(`Voted for fee: ${fee} with ${amount} UGTs.`);
+    }
+});
+
+async function vote() {
+    const voteFee = document.querySelector("#vote-fee-index").value;
+    const voteAmount = parseInt(document.querySelector("#vote-amount-index").value);
+
+    if (
+        ethers.BigNumber.from(await token.balanceOf(await signer.getAddress())).lt(voteAmount)
+    ) {
+        alert("Not enough UGT to vote!");
+        return;
+    }
+
+    if (voteAmount < 0) {
+        alert("Vote amount can't go below 0.");
+        return;
+    }
+
+    if (parseInt(voteFee) < 0) {
+        alert("Fee can't go below 0.");
+        return;
+    }
+
+    await token.approve(
+        rice.address, 
+        ethers.BigNumber.from( BigInt( parseUGT(voteAmount) ) )
+    );
+
+    token.on("Approval", async (owner, spender, value) => {
+        await rice.voteOnCallerFee(
+            ethers.utils.parseEther(voteFee),
+            ethers.BigNumber.from( BigInt( parseUGT(voteAmount) ) )
+        );
+    });
+}
+
+const voteButton = document.querySelector("#vote-button");
+
+voteButton.onclick = async () => {
+    await vote();
+}
+
+
+// Withdraw
+
+rice.on("WithdrawOnCallerFee", async (fee, participant) => {
+    if (participant === await signer.getAddress()) {
+        alert(`Withdrawed from voted fee: ${fee}.`);
+    }
+});
+
+async function withdraw() {
+    const votedFee = document.querySelector("#voted-fee-index").value;
+
+    await rice.withdrawOnCallerFee(ethers.utils.parseEther(votedFee));
+}
+
+const withdrawButton = document.querySelector("#withdraw-button");
+
+withdrawButton.onclick = async () => {
+    await withdraw();
+}
+
+
+/*
+    Utilities
+*/
+
+function parseUGT(amount) {
+    return amount * 10 ** decimals;
+}
